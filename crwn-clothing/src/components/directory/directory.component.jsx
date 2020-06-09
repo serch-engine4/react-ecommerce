8b247d00a48d9553eec9ab57728eced8788ebc9e
serch-engine4 <sergio.martinezc@outlook.com>
2020-06-09 14:45:16 -0500

    update our menu-items to navigate withRouter HOC

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -15,7 +15,7 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                     id: 1,
                     size: 'large',
-                    linkUrl: 'shop/hats'
+                    linkUrl: 'hats'
                 },
                 {
                     title: 'jackets',
@@ -36,7 +36,6 @@ class Directory extends React.Component {
                     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                     size: 'large',
                     id: 4,
-                    size: 'large',
                     linkUrl: 'shop/womens'
                 },
                 {
@@ -54,8 +53,8 @@ class Directory extends React.Component {
         return (
         <div className="directory-menu">
             {
-                this.state.sections.map(({ title, imageUrl, id, size }) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))
             }
         </div>)
