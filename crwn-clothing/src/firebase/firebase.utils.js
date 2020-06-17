76e1fe52914d2338b397d23cb098300a6d22580a
serch-engine4 <sergio.martinezc@outlook.com>
2020-06-17 00:34:58 -0500

    added redux, and implemented userReducer and userActions to header and app components

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -13,7 +13,7 @@ const config = {
     measurementId: "G-DGLGT0MZG4"
 };
 
-export const createUserProfileDocument = async (userAuth, ...additionalData) => {
+export const createUserProfileDocument = async (userAuth, additionalData) => {
     if(!userAuth) return;
 
     const userRef = firestore.doc(`users/${userAuth.uid}`);
